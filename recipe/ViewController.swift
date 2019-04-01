d26d1f9711975249464e515968feb74e7c7c6419
Admin <admin@MAC13.local>
2019-04-01 12:06:55 +0700

    first commit

---
+++ recipe/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  recipe
+//
+//  Created by Admin on 26/3/2562 BE.
+//  Copyright © 2562 Admin. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+
+struct Recipe: Decodable {
+    let title: String?
+    let results: [Results]
+}
+
+struct Results: Decodable {
+    let title: String?
+    let href: String?
+    let ingredients: String
+    let thumbnail: String
+}
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var TextViews: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        TextViews.text = ""
+        let jsonURLString = "http://www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=1"
+        guard let urlObj = URL(string: jsonURLString) else {return}
+        
+        URLSession.shared.dataTask(with: urlObj) {(data, responds, err) in
+            guard let data = data else {return}
+            do {
+                let countries = try JSONDecoder().decode(Recipe.self, from: data)
+                
+                for countrys in countries.results {
+                    self.TextViews.text.append("Food name : \(countrys.title!) \n")
+                    self.TextViews.text.append("Material : \(countrys.ingredients) \n")
+                    self.TextViews.text.append("================================ \n")
+                }
+                
+            } catch {
+                print(error)
+            }
+        }.resume()
+    }
+
+    
+}
+
